d7a519e4feba331126d08ebb17df5396c0571598
Alfredo9909 <alfredo10000z@gmail.com>
2023-10-07 12:43:23 -0600

    se agregfo

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -10,6 +10,7 @@ class Cliente extends Model
     use HasFactory;
     protected $fillable = ['name', 'last_name', 'first_name', 'email', 'password', 
     'age', 'sex', 'city', 'postal_zip'];
+    
 
 
 }
